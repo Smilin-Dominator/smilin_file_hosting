13209824ae3e1cacb5f2cc873f17ae553e3920c8
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-25 00:15:35 +0530

    fix: Added an encrypt_string function

--- client/cryptography.py
+++ client/cryptography.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 class Crypto:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.pub = PublicKey(1, 1)
         self.priv = PrivateKey(1, 1, 1, 1, 1)
 
-    def create_new_keys(self):
+    def create_new_keys(self) -> None:
         pub, priv = newkeys(4096)
         Path("credentials").mkdir()
         with open("./credentials/public.pem", "w+") as w:
@@ -20,12 +20,15 @@ class Crypto:
         self.priv = priv
         self.pub = pub
 
-    def load_existing_keys(self):
+    def load_existing_keys(self) -> None:
         with open("./credentials/public.pem", "rb") as r:
             self.priv = PrivateKey.load_pkcs1(r.read())
         with open("./credentials/private.pem", "rb") as r:
             self.pub = PublicKey.load_pkcs1(r.read())
 
-    def decrypt_string(self, string: bytes):
+    def decrypt_string(self, string: bytes) -> str:
         text = decrypt(string, self.priv)
         return text.decode('utf-8')
+
+    def encrypt_string(self, string: str) -> str:
+        return str(encrypt(string.encode('utf-8'), self.pub))
