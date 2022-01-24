c96583e6092dd8dc0b654a99ba4a4ac904f6be7f
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-25 00:45:44 +0530

    chore: Added the licenses to cryptography.py and connector.py

--- client/connector.py
+++ client/connector.py
@@ -1,6 +1,25 @@
+# connector.py -> Interacts with the API to get the files
+"""
+    Smilin' File Hosting - Hosting Files With A Smile
+    Copyright (C) 2022 Devisha Padmaperuma
+
+    Smilin' File Hosting is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Smilin' File Hosting is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from requests import get, post
 from furl import furl
 from shutil import copyfileobj
+from pathlib import Path
 
 
 class API:
