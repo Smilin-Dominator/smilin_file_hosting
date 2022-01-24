67f6b813ffef6c08032eb8611a13697578e0018b
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-24 14:47:46 +0530

    feat: Added Licenses

--- server/main.py
+++ server/main.py
@@ -1,3 +1,21 @@
+# main.py -> The main script that defines the API route
+"""
+    Smilin' File Hosting - Hosting Files With A Smile
+    Copyright (C) 2022 Devisha Padmaperuma
+
+    Smilin' File Hosting is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Smilin' File Hosting is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from fastapi import FastAPI
 
 app = FastAPI()
