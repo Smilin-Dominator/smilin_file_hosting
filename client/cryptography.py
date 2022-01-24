557ed6988f0f8bd32c722830c39b054481c93897
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-25 00:08:07 +0530

    fix: Typo

--- client/cryptography.py
+++ client/cryptography.py
@@ -10,7 +10,7 @@ class Crypto:
 
     def create_new_keys(self):
         pub, priv = newkeys(4096)
-        Path("credetials").mkdir()
+        Path("credentials").mkdir()
         with open("./credentials/public.pem", "w") as w:
             key_string = pub.save_pkcs1()
             w.write(key_string.decode('utf-8'))
