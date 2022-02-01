b37d5975ea0ee15f16f57b826e3d841431c4edee
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-02-01 14:49:55 +0530

    feat: Converted credentials into a class

--- client/main.py
+++ client/main.py
@@ -9,6 +9,85 @@ from connector import API
 from cryptography import Crypto
 
 
+class CredentialsUI(QMainWindow):
+
+    def __init__(self):
+
+        # Declaration
+        self.token_input: QLineEdit = None
+        self.link_input: QLineEdit = None
+        self.email_input: QLineEdit = None
+        self.register_button: QPushButton = None
+        self.connect_button: QPushButton = None
+        self.credentials_status: QLabel = None
+
+        # Initial
+        super(CredentialsUI, self).__init__()
+        uic.loadUi("credentials.ui", self)
+        self.setWindowTitle("Smilin' File Client - Credentials")
+
+        # Post Initial
+        self.connect_button.clicked.connect(self.credentials_connect)
+        self.register_button.clicked.connect(self.credentials_register)
+
+    def credentials_connect(self):
+        text = [self.token_input.text(), self.link_input.text(), self.email_input.text()]
+        check = [i for i in text if i != ""]
+        if len(check) != 3:
+            self.credentials_status.setText("Not Enough Arguments!")
+        else:
+            self.write_file(*text)
+
+    def credentials_register(self):
+        if self.link_input.text() == "":
+            self.credentials_status.setText("Link Is Empty!")
+        else:
+            token = api.register(self.link_input.text())
+            self.credentials_status.setText("Successfully Registered!")
+            self.token_input.setText(token)
+
+    def write_file(self, token: str, link: str, email: str) -> None:
+        global crypto, main_window
+        out = {
+            "token": token,
+            "link": link,
+            "email": email
+        }
+        creds = Path("credentials")
+        creds.mkdir() if not creds.exists() else None
+        with open("credentials/config.json", "w") as w:
+            w.write(dumps(out, indent=4))
+            w.flush()
+            w.close()
+        api = API(server=link, username=token, email=email)
+        if not api.test_connection():
+            self.credentials_status.setText("Connection Failed!")
+            self.show()
+        else:
+            crypto.setup_gpg(email)
+            self.close()
+            main_window.show()
+
+    def read_file(self) -> bool:
+        global api
+        try:
+            with open("credentials/config.json", "r") as r:
+                js = loads(r.read())
+                api = API(server=js["link"], username=js["token"], email=js["email"])
+                if not api.test_connection():
+                    self.credentials_status.setText("Connection Failed!")
+                    return False
+                crypto.setup_gpg(js["email"])
+                main_window.show()
+                return True
+        except FileNotFoundError:
+            self.credentials_status.setText("File Not Found!")
+            return False
+        except KeyError:
+            self.credentials_status.setText("Not Enough Options!")
+            return False
+
+
 class MainUI(QMainWindow):
 
     def __init__(self):
@@ -45,87 +124,11 @@ api = API("", "", "")
 crypto = Crypto()
 app = QApplication(argv)
 
-credentials_window = uic.loadUi("credentials.ui")
+credentials_window = CredentialsUI()
 main_window = MainUI()
 
-# Credentials Variables (Necessary)
-token_input: QLineEdit = credentials_window.token_input
-link_input: QLineEdit = credentials_window.link_input
-email_input: QLineEdit = credentials_window.email_input
-register_button: QPushButton = credentials_window.register_button
-connect_button: QPushButton = credentials_window.connect_button
-credentials_status: QLabel = credentials_window.credentials_status
-
-
-# ------ Credentials Functions -------------------- #
-
-def credentials_connect():
-    text = [token_input.text(), link_input.text(), email_input.text()]
-    check = [i for i in text if i != ""]
-    if len(check) != 3:
-        credentials_status.setText("Not Enough Arguments!")
-    else:
-        write_file(*text)
-
-
-def credentials_register():
-    if link_input.text() == "":
-        credentials_status.setText("Link Is Empty!")
-    else:
-        token = api.register(link_input.text())
-        credentials_status.setText("Successfully Registered!")
-        token_input.setText(token)
-
-
-connect_button.clicked.connect(credentials_connect)
-register_button.clicked.connect(credentials_register)
-
-
-# ------------ Config File ------------------- #
-def write_file(token: str, link: str, email: str) -> None:
-    out = {
-        "token": token,
-        "link": link,
-        "email": email
-    }
-    creds = Path("credentials")
-    creds.mkdir() if not creds.exists() else None
-    with open("credentials/config.json", "w") as w:
-        w.write(dumps(out, indent=4))
-        w.flush()
-        w.close()
-    api = API(server=link, username=token, email=email)
-    if not api.test_connection():
-        credentials_status.setText("Connection Failed!")
-        credentials_window.show()
-    else:
-        crypto.setup_gpg(email)
-        credentials_window.close()
-        main_window.show()
-
-
-def read_file() -> bool:
-    global api
-    try:
-        with open("credentials/config.json", "r") as r:
-            js = loads(r.read())
-            api = API(server=js["link"], username=js["token"], email=js["email"])
-            if not api.test_connection():
-                credentials_status.setText("Connection Failed!")
-                return False
-            crypto.setup_gpg(js["email"])
-            main_window.show()
-            return True
-    except FileNotFoundError:
-        credentials_status.setText("File Not Found!")
-        return False
-    except KeyError:
-        credentials_status.setText("Not Enough Options!")
-        return False
-
-
 if __name__ == "__main__":
-    options = read_file()
+    options = credentials_window.read_file()
     if not options:
         credentials_window.show()
     else:
